4df194e76667568b244e25866529583fab500353
roger gomez <rgomez@jumpmath.es>
2023-09-28 15:21:45 +0200

    import dotenv in function app

--- functions/app.js
+++ functions/app.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express from "express";
 import { renderToPipeableStream } from "react-dom/server";
 import { fillJSXwithClientComponents } from "../src/client/utils/index.js";
